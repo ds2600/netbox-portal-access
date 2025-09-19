ad57b4d153c3882124ab5260e831f52d92a45331
ds2600 <bill@ishiganto.com>
2025-09-19 16:42:31 -0400

    attempted to add tlm

--- netbox_portal_access/navigation.py
+++ netbox_portal_access/navigation.py
@@ -1,23 +1,22 @@
 
+from django.conf import settings
 from netbox.plugins import PluginMenuItem, PluginMenu, PluginMenuButton
 from django.urls import reverse
 
-menu = PluginMenu(
-    label="Portal Access",
-    groups=(
-        ("Portal Access", (
-            PluginMenuItem(
-                link="plugins:netbox_portal_access:portal_list",
-                link_text="Portals",
-            ),
-            PluginMenuItem(
-                link="plugins:netbox_portal_access:vendorrole_list",
-                link_text="Vendor Roles",
-            ),
-            PluginMenuItem(
-                link="plugins:netbox_portal_access:accessassignment_list",
-                link_text="Access Assignments",
-            ),
-        )),
-    )
+items = (
+        PluginMenuItem(link="plugins:netbox_portal_access:portal_list", link_text="Portals"),
+        PluginMenuItem(link="plugins:netbox_portal_access:vendorrole_list", link_text="Vendor Roles"),
+        PluginMenuItem(link="plugins:netbox_portal_access:accessassignment_list", link_text="Access Assignments"),
 )
+
+cfg = settings.PLUGINS_CONFIG.get("netbox_portal_access", {})
+
+if cfg.get("top_level_menu", True):
+    menu = PluginMenu(
+        label="Portal Access",
+        groups=(("Portal Access", items),),
+        icon_class="mdi mdi-shield-key"
+    )
+else:
+    menu_items = items
+
