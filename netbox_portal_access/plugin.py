6b1c5d8b9d0a337cc9a01e406e0cdbda77d9a876
ds2600 <bill@ishiganto.com>
2025-09-19 16:28:11 -0400

    added default top level menu

--- netbox_portal_access/plugin.py
+++ netbox_portal_access/plugin.py
@@ -4,12 +4,14 @@ from netbox.plugins import PluginConfig
 class PortalAccessConfig(PluginConfig):
     name = "netbox_portal_access"
     label = "netbox_portal_access"
-    verbose_name = "Portal Access"
+    verbose_name = "NetBox Portal Access Tracker"
     version = "0.1.0"
     base_url = "portal-access"
-    min_version = "3.6.0"
+    min_version = "4.0.0"
     max_version = "4.4.1"
     required_settings = []
-    default_settings = {}
+    default_settings = {
+        'top_level_menu': True
+    }
 
 config = PortalAccessConfig
