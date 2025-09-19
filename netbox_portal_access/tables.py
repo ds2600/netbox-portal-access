b39a767a19d84e20977a37552a2bc47f83dd5dff
ds2600 <bill@ishiganto.com>
2025-09-19 16:10:06 -0400

    added django-tables2 support

--- netbox_portal_access/tables.py
+++ netbox_portal_access/tables.py
@@ -1,30 +1,31 @@
 
-from netbox.tables import NetBoxTable, columns
+from netbox.tables import NetBoxTable
+from django_tables2 import Column, BooleanColumn, DateColumn
 from .models import Portal, VendorRole, AccessAssignment
 
 class PortalTable(NetBoxTable):
-    name = columns.Column(linkify=True)
-    vendor = columns.TemplateColumn(template_code="{{ record.vendor }}")
-    base_url = columns.Column()
+    name = Column(linkify=True)
+    vendor = Column(accessor="vendor", verbose_name="Vendor") 
+    base_url = Column()
     class Meta(NetBoxTable.Meta):
         model = Portal
         fields = ("pk", "name", "vendor", "base_url", "created", "last_updated")
 
 class VendorRoleTable(NetBoxTable):
-    portal = columns.Column(linkify=True)
-    name = columns.Column(linkify=True)
-    category = columns.Column()
+    portal = Column(linkify=True)
+    name = Column(linkify=True)
+    category = Column()
     class Meta(NetBoxTable.Meta):
         model = VendorRole
         fields = ("pk", "portal", "name", "category", "created", "last_updated")
 
 class AccessAssignmentTable(NetBoxTable):
-    portal = columns.Column(linkify=True)
-    role = columns.Column(linkify=True)
-    user = columns.Column(linkify=True)
-    active = columns.BooleanColumn()
-    last_verified = columns.DateColumn()
-    expires_on = columns.DateColumn()
+    portal = Column() 
+    role = Column()
+    user = Column() 
+    active = BooleanColumn()
+    last_verified = DateColumn()
+    expires_on = DateColumn()
     class Meta(NetBoxTable.Meta):
         model = AccessAssignment
         fields = ("pk", "user", "portal", "role", "active", "last_verified", "expires_on", "created", "last_updated")
