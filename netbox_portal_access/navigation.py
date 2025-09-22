d7e690628bb58f3f6dba0d37adcfef6dc9e66b70
ds2600 <bill@ishiganto.com>
2025-09-22 14:59:04 -0400

    added perms to menu

--- netbox_portal_access/navigation.py
+++ netbox_portal_access/navigation.py
@@ -3,9 +3,21 @@ from netbox.plugins import PluginMenuItem, PluginMenu, PluginMenuButton
 from django.urls import reverse
 
 items = (
-        PluginMenuItem(link="plugins:netbox_portal_access:portal_list", link_text="Portals"),
-        PluginMenuItem(link="plugins:netbox_portal_access:vendorrole_list", link_text="Vendor Roles"),
-        PluginMenuItem(link="plugins:netbox_portal_access:accessassignment_list", link_text="Access Assignments"),
+        PluginMenuItem(
+            link="plugins:netbox_portal_access:portal_list", 
+            link_text="Portals",
+            permissions=["netbox_portal_access.view_portal"],
+        ),
+        PluginMenuItem(
+            link="plugins:netbox_portal_access:vendorrole_list", 
+            link_text="Vendor Roles",
+            permissions=["netbox_portal_access.view_vendorrole"],
+        ),
+        PluginMenuItem(
+            link="plugins:netbox_portal_access:accessassignment_list", 
+            link_text="Access Assignments",
+            permissions=["netbox_portal_access.view_accessassignment"],
+        ),
 )
 
 menu = PluginMenu(
