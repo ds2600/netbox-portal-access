b1c2574dd37f777dcd3d06bd57d97757a04b63c8
ds2600 <bill@ishiganto.com>
2025-09-22 10:24:18 -0400

    removed plugin.py

--- netbox_portal_access/plugin.py
+++ netbox_portal_access/plugin.py
@@ -1,15 +1,1 @@
 
-from netbox.plugins import PluginConfig
-
-class PortalAccessConfig(PluginConfig):
-    name = "netbox_portal_access"
-    verbose_name = "NetBox Portal Access Tracker"
-    description = "A plugin to track portal access in NetBox."
-    version = "0.1.0"
-    base_url = "portal-access"
-    min_version = "4.0.0"
-    top_level_menu = True
-    required_settings = []
-    default_settings = {}
-
-config = PortalAccessConfig
