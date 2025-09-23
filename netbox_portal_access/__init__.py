3f4bcf5148081a9e8444c275538dc8e1d7db3799
ds2600 <bill@ishiganto.com>
2025-09-23 07:42:56 -0400

    attempting to rename to template_content

--- netbox_portal_access/__init__.py
+++ netbox_portal_access/__init__.py
@@ -1,1 +1,16 @@
-from .plugin import config
+from netbox.plugins import PluginConfig
+
+class PortalAccessConfig(PluginConfig):
+    name = "netbox_portal_access"
+    verbose_name = "NetBox Portal Access Tracker"
+    description = "A plugin to track portal access in NetBox."
+    version = "0.1.0"
+    base_url = "portal-access"
+    min_version = "4.4.1"
+    top_level_menu = True
+    required_settings = []
+    default_settings = {
+        "stale_days": 90,
+        "expiring_soon_days": 14,
+    }
+config = PortalAccessConfig
