a4a4baf04669bc52f9d53f45ac21779f393af445
ds2600 <bill@ishiganto.com>
2025-09-23 07:32:26 -0400

    trying another wrong fix

---
+++ netbox_portal_access/plugin.py
@@ -0,0 +1,23 @@
+from netbox.plugins import PluginConfig
+from .template_extensions import ProviderPortalAccess, TenantPortalAccess, UserPortalAccess
+
+class PortalAccessConfig(PluginConfig):
+    name = "netbox_portal_access"
+    verbose_name = "NetBox Portal Access Tracker"
+    description = "A plugin to track portal access in NetBox."
+    version = "0.1.0"
+    base_url = "portal-access"
+    min_version = "4.4.1"
+    top_level_menu = True
+    required_settings = []
+    default_settings = {
+        "stale_days": 90,
+        "expiring_soon_days": 14,
+    }
+    template_extensions = [
+        ProviderPortalAccess,
+        TenantPortalAccess,
+        UserPortalAccess,
+    ]
+
+config = PortalAccessConfig
