ac5d36f6df738baf4c62c9e0bcd0a42727edcc60
ds2600 <bill@ishiganto.com>
2025-09-29 10:12:24 -0400

    trying a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='netbox-portal-access',
+    version='0.1.0',
+    description='NetBox plugin to track multi-vendor portal access & roles.',
+    author='William Bunce',
+    license='MIT',
+    packages=['netbox_portal_access'],
+    install_requires=[
+        'netbox>=4.4.1',
+    ],
+)
