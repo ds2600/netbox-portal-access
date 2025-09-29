a8cde83745e17a464abaf25e167302889d6dde71
ds2600 <bill@ishiganto.com>
2025-09-29 09:02:44 -0400

    updated plugin name

--- netbox_portal_access/__init__.py
+++ netbox_portal_access/__init__.py
@@ -2,8 +2,8 @@ from netbox.plugins import PluginConfig
 
 class PortalAccessConfig(PluginConfig):
     name = "netbox_portal_access"
-    verbose_name = "NetBox Portal Access Tracker"
-    description = "A plugin to track portal access in NetBox."
+    verbose_name = "NetBox Portal Access"
+    description = "A plugin to track vendor portal access in NetBox."
     version = "0.1.0"
     base_url = "portal-access"
     min_version = "4.4.1"
